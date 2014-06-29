04a0d5fa89a01cda2af944b37250bb68275b20bc
Charles Zhu <scorpiozj@gmail.com>
2014-06-29 22:01:56 +0800

    initial add ZZTestView

---
+++ WWDC2014_221/WWDC2014_221/ZZTestView.m
@@ -0,0 +1,31 @@
+//
+//  ZZTestView.m
+//  WWDC2014_221
+//
+//  Created by scorpiozj on 6/29/14.
+//  Copyright (c) 2014 Ch J. All rights reserved.
+//
+
+#import "ZZTestView.h"
+
+@implementation ZZTestView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
