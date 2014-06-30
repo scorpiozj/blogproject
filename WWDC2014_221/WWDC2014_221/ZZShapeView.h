77a36226b7d4128f8df949db31308aa1276a418d
scorpiozj <scorpiozj@gmail.com>
2014-06-30 14:56:25 +0800

    initial Add ZZShapeView

---
+++ WWDC2014_221/WWDC2014_221/ZZShapeView.h
@@ -0,0 +1,13 @@
+//
+//  ZZShapeView.h
+//  WWDC2014_221
+//
+//  Created by Zhu J on 6/30/14.
+//  Copyright (c) 2014 Ch J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZZShapeView : UIView
+
+@end
---
+++ WWDC2014_221/WWDC2014_221/ZZShapeView.m
@@ -0,0 +1,31 @@
+//
+//  ZZShapeView.m
+//  WWDC2014_221
+//
+//  Created by Zhu J on 6/30/14.
+//  Copyright (c) 2014 Ch J. All rights reserved.
+//
+
+#import "ZZShapeView.h"
+
+@implementation ZZShapeView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
